e30b0206611bdf4e57b5629e3daa75857cf441e4
Wes Freeman <wes@skeweredrook.com>
2013-08-22 01:45:38 -0400

    got a bit farther.

--- src/main/scala/rdf2neo.scala
+++ src/main/scala/rdf2neo.scala
@@ -6,14 +6,35 @@ import org.neo4j.graphdb._
 import collection.JavaConverters._
 
 object Main extends App {
-  val graph = new EmbeddedGraphDatabase(Settings.outputGraphPath)
+  val graph = new GraphDatabaseFactory()
+    .newEmbeddedDatabase(Settings.outputGraphPath)
   val rootzip = new java.util.zip.ZipFile(Settings.zippedTurtleFile)
   val entries = rootzip.entries.asScala
   entries.foreach { e =>
     import java.io.{BufferedReader, InputStreamReader}
     val is = rootzip.getInputStream(e)
     val in = new BufferedReader(new InputStreamReader(is))
-    Stream.continually(in.readLine()).takeWhile(_ != null).foreach(println(_))
+    Stream.continually(in.readLine()).takeWhile(_ != null).foreach(processTurtle(_))
+  }
+  graph.shutdown
+
+  def processTurtle(turtle:String) = {
+    if(turtle.startsWith("@base")) {
+      // do we need to handle these? 
+    } else if (turtle.startsWith("@prefix")) {
+      // do we need to handle these?
+    } else if (turtle.startsWith("#")) { 
+      // definitely don't need to handle these
+    } else {
+      val (subj, pred, obj) = turtle.substring(.split("\\s+")
+      val tx = graph.beginTx
+      try {
+        // http://docs.neo4j.org/chunked/milestone/tutorials-java-embedded-new-index.html
+        tx.success
+      } finally {
+        tx.finish
+      }
+    }
   }
 
   def relType(str:String):RelationshipType = DynamicRelationshipType.withName(str)
