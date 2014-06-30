09b533cdbe49772d07a87ff77ca656862f412d64
Wes Freeman <wes@skeweredrook.com>
2014-06-30 02:10:46 -0400

    fixing binary search

--- src/main/scala/IdMap.scala
+++ src/main/scala/IdMap.scala
@@ -4,19 +4,22 @@ import java.util
 
 class IdMap {
   def apply:IdMap = new IdMap()
-  var arr = Array.fill[Long](100000000)(-1)
+  var arr = Array.fill[Long](100000000)(Long.MaxValue)
   var idx = 0
+  var flag = false
   def put(mid:Long) = {
     arr(idx) = mid
     idx += 1
   }
   def get(mid:Long):Long = {
-    util.Arrays.binarySearch(arr, mid).toLong
+    if(!flag) throw new Exception("need to call done() first.")
+    util.Arrays.binarySearch(arr, mid, 0, idx).toLong
   }
   def contains(mid:Long):Boolean = {
     get(mid) > 0
   }
   def done = {
     util.Arrays.sort(arr)
+    flag = true
   }
 }
